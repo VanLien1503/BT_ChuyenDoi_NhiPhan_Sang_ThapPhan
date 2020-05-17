1977556434a327e5cecb6cdc1edd999b3a9cbe7f
VanLien1503 <hoanglien89nd@gmail.com>
2020-05-17 13:56:46 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 50;
+        int temp;
+        Stack<Integer> stack = new Stack<>();
+        System.out.print("Before: " + number);
+        System.out.print("\nAfter: ");
+        while (number != 0) {
+            temp = number % 2;
+            stack.push(temp);
+            number = number / 2;
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
